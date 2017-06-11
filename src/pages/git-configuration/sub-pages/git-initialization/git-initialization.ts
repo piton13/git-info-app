5ebd29d6e2432527f2b674c4b4eae2742f6daa05
Igor Mykhaylyuk <igor13luk@gmail.com>
2017-06-11 16:04:05 +0300

    add loader for http requests

--- src/pages/git-configuration/sub-pages/git-initialization/git-initialization.ts
+++ src/pages/git-configuration/sub-pages/git-initialization/git-initialization.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpProvider } from '../../../../providers/http/http';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'git-initialization-page',
@@ -11,13 +12,23 @@ import { HttpProvider } from '../../../../providers/http/http';
 export class GitInitializationPage implements OnInit, OnDestroy {
   public pageData: { title: string, commands: Object[] };
 
+  private loading: any;
   private subscription: any;
 
-  constructor(private httpProvider: HttpProvider) {}
+  constructor(private httpProvider: HttpProvider, private loadingCtrl: LoadingController) {
+    this.loading = this.loadingCtrl.create({
+      content: `<ion-spinner></ion-spinner>`
+    });
+  }
 
   ngOnInit() {
+    this.loading.present();
     this.subscription = this.httpProvider.getInitializationCommands().subscribe(data => {
       this.pageData = data;
+    }, error => {
+      console.error('Error:' + error);
+    }, () => {
+      this.loading.dismiss();
     });
   }
 
--- src/pages/git-configuration/sub-pages/remote-linking/remote-linking.ts
+++ src/pages/git-configuration/sub-pages/remote-linking/remote-linking.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpProvider } from '../../../../providers/http/http';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'git-remote-linking-page',
@@ -7,13 +8,23 @@ import { HttpProvider } from '../../../../providers/http/http';
 })
 export class GitRemoteLinkingPage implements OnInit, OnDestroy {
   public pageData: { title: string, commands: Object[]};
+  private loading: any;
   private subscription: any;
 
-  constructor(private httpProvider: HttpProvider) {}
+  constructor(private httpProvider: HttpProvider, private loadingCtrl: LoadingController) {
+    this.loading = this.loadingCtrl.create({
+      content: `<ion-spinner></ion-spinner>`
+    });
+  }
 
   ngOnInit() {
+    this.loading.present();
     this.subscription = this.httpProvider.getRemoteLinkingCommands().subscribe(data => {
       this.pageData = data;
+    }, err => {
+      console.error('Error: ' + err);
+    }, () => {
+      this.loading.dismiss();
     });
   }
 
